e05ab34d9293c16adefd47c35422c64ebbe0e317
bernard-williams <tomas.marcanik@gmail.com>
2017-09-08 15:20:26 +0200

    New version of my_script

--- my_script.js
+++ my_script.js
@@ -1,20 +1,24 @@
 // ==UserScript==
-// @id          My_personal_script@https://raw.githubusercontent.com/bernard-williams/Spoon-Knife
+// @id          My_personal_script@https://github.com/bernard-williams/Spoon-Knife
 // @name        My personal script
-// @namespace   https://www.maturitnitabla.cz/scripts
+// @namespace   https://github.com/bernard-williams/Spoon-Knife
 // @description Muj osobni testovaci script
 // @author      tomas.marcanik@aukro.cz
 // @license     GNU GPLv3
-// @supportURL  https://raw.githubusercontent.com/bernard-williams/Spoon-Knife
-// @homepageURL https://raw.githubusercontent.com/bernard-williams/Spoon-Knife
-// @downloadURL https://raw.githubusercontent.com/bernard-williams/Spoon-Knife/master/my_script.js
-// @updateURL   https://raw.githubusercontent.com/bernard-williams/Spoon-Knife/master/my_script.js
+// @supportURL  https://github.com/bernard-williams/Spoon-Knife
+// @homepageURL https://github.com/bernard-williams/Spoon-Knife
+// @downloadURL https://github.com/bernard-williams/Spoon-Knife/raw/master/my_script.js
+// @updateURL   https://github.com/bernard-williams/Spoon-Knife/raw/master/my_script.js
 // @version     1.0
 // @grant       none
 // @run-at      document-end
 // @include     https://github.com/*
 // @include     https://www.github.com/*
 // ==/UserScript==
+/* global unsafeWindow */
+
+(function(unsafeWindow) {
 
 alert("jedu");
 
+})(typeof unsafeWindow !== "undefined" ? unsafeWindow : window);
