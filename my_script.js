0acfa57736fcd15cd603cb04f50fbc8a3fefa1aa
bernard-williams <tomas.marcanik@gmail.com>
2017-09-08 15:15:29 +0200

    File for Tampermonkey

---
+++ my_script.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @id          My_personal_script@https://raw.githubusercontent.com/bernard-williams/Spoon-Knife
+// @name        My personal script
+// @namespace   https://www.maturitnitabla.cz/scripts
+// @description Muj osobni testovaci script
+// @author      tomas.marcanik@aukro.cz
+// @license     GNU GPLv3
+// @supportURL  https://raw.githubusercontent.com/bernard-williams/Spoon-Knife
+// @homepageURL https://raw.githubusercontent.com/bernard-williams/Spoon-Knife
+// @downloadURL https://raw.githubusercontent.com/bernard-williams/Spoon-Knife/master/my_script.js
+// @updateURL   https://raw.githubusercontent.com/bernard-williams/Spoon-Knife/master/my_script.js
+// @version     1.0
+// @grant       none
+// @run-at      document-end
+// @include     https://maturitnitabla.cz/*
+// @include     https://www.maturitnitabla.cz/*
+// ==/UserScript==
+
+alert("jedu");
+
